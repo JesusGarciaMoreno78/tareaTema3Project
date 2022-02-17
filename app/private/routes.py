be1ba5c591768d465ae43c69562d356328d40066
jesus <3607234@alu.murciaeduca.es>
2022-02-17 18:25:28 +0100

    importamos generarForm y lo pintamos

--- app/private/routes.py
+++ app/private/routes.py
@@ -1,10 +1,12 @@
-from flask import render_template
+from flask import render_template, request
 
 from . import private
 from .models import Cliente
+from .forms import FiltroCliente
 
 @private.route("/indexcliente/", methods=["GET","POST"])
 def indexcliente():
+    form = FiltroCliente(request.form)
     clientes = Cliente.query.all()
-    return render_template("indexcliente.html", clientes=clientes)
+    return render_template("indexcliente.html", clientes=clientes, form=form)
 
