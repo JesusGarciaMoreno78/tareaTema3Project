ba3021d8119598b396e5739ba4e22f8c0aeb75ec
jesus <3607234@alu.murciaeduca.es>
2022-02-17 18:20:16 +0100

    app.secret_key="clave"

--- app/private/models.py
+++ app/private/models.py
@@ -4,7 +4,7 @@ from app import db
 
 class Cliente(db.Model):
     dni = db.Column(db.String(10), primary_key=True)
-    nombre = db.Column(db.String(20), required=True)
-    apellidos = db.Column(db.String(50), required=True)
-    imagen = db.Column(db.String, required=True)
+    nombre = db.Column(db.String(20), nullable=False)
+    apellidos = db.Column(db.String(50),  nullable=False)
+    imagen = db.Column(db.String(), nullable=False)
 
