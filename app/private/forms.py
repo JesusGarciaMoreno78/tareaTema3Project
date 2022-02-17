4da135a9a940216164ccb1a309f7fc2456d273d0
jesus <3607234@alu.murciaeduca.es>
2022-02-17 17:28:54 +0100

    Clase FiltroCliente

--- app/private/forms.py
+++ app/private/forms.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired, Length
 
 class FiltroCliente(FlaskForm):
     dni = StringField(label="Dni", validators=[
-        DataRequired("Este campo no puede estar vacío")
+        DataRequired("Este campo no puede estar vacío"),
         Length(message="La longitud no puede ser superior a 10 caracteres", max=10)
-    ]
+    ])
