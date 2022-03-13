34bd032d98f44444155f0225b1d56443146e3f06
jesus <3607234@alu.murciaeduca.es>
2022-03-13 09:44:28 +0100

    login

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,32 @@
-#Para poder usar Flask devemos incluirlo en el archivo requerimens para poder instalarlo
+# Para poder usar Flask devemos incluirlo en el archivo requerimens para poder instalarlo
 
 from flask import Flask, render_template, request
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-#pag 23 pps0303-Entrada de aula virtual
+# pag 23 pps0303-Entrada de aula virtual
 app = Flask(__name__)
-app.secret_key="clave"
-#Establecer la cadena de conexion
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost:5432/tareaTema3Projet'
-#desactivamos la gestion de notificaciones de SQLAlchemy
+# en vez de clave poner una clave compleja
+app.secret_key = "clave"
+# Establecer la cadena de conexion
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost:5432/tareaTema3Project'
+# desactivamos la gestion de notificaciones de SQLAlchemy
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#Instanciamos un objeto de la cxlase SQLAlchemy
+# Instanciamos un objeto de la cxlase SQLAlchemy
 db = SQLAlchemy(app)
 
-#pag 25 pps0303-Entrada de aula virtual
-#Instanciar un objeto de la classe Migrate
-migrate = Migrate(app,db)
-# Por qué si subo estos import con los de arriba no funciana???????
+# pag 25 pps0303-Entrada de aula virtual
+# Instanciar un objeto de la classe Migrate
+migrate = Migrate(app, db)
+# Por qué si subo estos import con los de arriba no funcioana???????
 from .public import public
 from .private import private
+from .login import login
+
 
 def create_app():
     # Úlimo paso Blueprint (registrarlo)
     app.register_blueprint(public)
     app.register_blueprint(private)
+    app.register_blueprint(login)
     return app
---
+++ app/login/routes.py
@@ -0,0 +1,42 @@
+import base64
+
+from flask import render_template, request, redirect, url_for
+from setuptools.command.dist_info import dist_info
+from werkzeug.datastructures import CombinedMultiDict
+
+from . import login
+from .forms import LogUsuarioForm, RegistroUsuarioForm
+from .models import Usuario
+
+PEEPER = "66a2b072-ae6a-4f49-8c3b-6668de2a2bc9"
+
+@login.route("/nuevoUsuario/", methods=["GET","POST"])
+def nuevoUsuario():
+    error = ""
+    form = RegistroUsuarioForm(request.form)
+    if form.validate_on_submit():
+        try:
+            usuario = Usuario()
+            usuario.username = form.username.data
+            usuario.nombre = form.nombre.data
+            usuario.apellidos = form.apellidos.data
+            usuario.set_password(form.password.data + PEEPER)
+            usuario.create()
+            return redirect(url_for('private.indexcliente'))
+        except Exception as e:
+            error = "No se ha podido iniciar sesión " + e.__str__()
+    return render_template("nuevoUsuario.html", form=form, error=error)
+
+@login.route("/login/", methods=["GET","POST"])
+def login():
+    error = ""
+    form = LogUsuarioForm(request.form)
+    if form.validate_on_submit():
+        username = form.username.data
+        usuario = Usuario.get_by_username(username)
+
+        if usuario and usuario.check_password(form.password.data + PEEPER):
+            return redirect(url_for("private.indexcliente"))
+        else:
+            error = "Usuario y/o contraseña incorrecta"
+    return render_template("login.html", form=form, error=error)
