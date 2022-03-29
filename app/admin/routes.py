5256aa1c69980b3c173faa25417231d67807f372
jesus <3607234@alu.murciaeduca.es>
2022-03-29 12:13:26 +0200

    creamos un routes.py en el paquete admin importamos el admin_required (nuestro decorador) y creamos una funcion que compruebe si somos admin

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,18 +1,14 @@
-from flask import render_template
+from flask import render_template, abort
 from flask_login import login_required, current_user
-from . import admin
-from ..auth.decorator import admin_required
 
-# Segundo paso Blueprint public > ultimo paso en app/_init_.py
-# Este es el decorador de Blueprint que nos llevará a la raiz del modulo public
-@admin.route('/adminindex')
-@login_required
-@admin_required
-def index():  # Eta función devuelve un render_template que debemos importar
-    return render_template('administracionindex.html')# render_template('index.html') lo que hace es buscar un archivo index.html y lo representa (para ello debemos importar la carpeta donde se encuentre)
+from . import admin
+from .auth.decorator import admin_required
+
 
 @admin.route('/adminindex/')
 @login_required
 @admin_required
-def adminindex():
-    return render_template(('adminindex.html'))
+def adminindex():  # put application's code here
+    # if not current_user.is_admin:
+    #     abort(401)
+    return render_template('adminindex.html')
